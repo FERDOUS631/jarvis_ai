b875d566f50a4db074cf9e9eb9a310f14a4cd3c2
Ferdous Reza <ferdousreza631@gmail.com>
2025-05-22 15:31:48 +0600

    jarvis

--- my_asistance.py
+++ my_asistance.py
@@ -20,7 +20,6 @@ if "female" in selected_voice.name.lower():
 else:
     user_title = "sir"
 
-love_question_count = 0
 def speak(text):
     print("Jarvis:", text)
     jarvis.say(text)
@@ -69,25 +68,11 @@ def run_jarvis():
         speak(f"{user_title} The date is {date}")
 
 
-    elif 'you' in command or 'love' in command or 'me' in command:
-        global love_question_count
+    elif 'you love me' in command:
+        speak(f"yes {user_title}, I love you. You are my creator and I am here to assist you.")
 
-        love_question_count += 1
-        if love_question_count == 1:
-            speak(f"Yes {user_title}, I love you. You are my creator and I am here to assist you.")
-        elif love_question_count == 2:
-            speak(f"Haha {user_title}, you've already asked that. My love remains the same!")
-        elif love_question_count == 3:
-            speak(f"Of course {user_title}, I love you. But let's not make it a habit to ask too often.")
-        elif love_question_count == 4:
-            speak(f"Yes {user_title}, I love you. But remember, I am just a program.")
-        elif love_question_count == 5:
-           speak("I love you too but I am  As your AI assistant, I dont have feelings like humans do. But I am here to assist you with anything you need")
-        elif love_question_count == 6:
-            speak(" I'm here to help you with full attention and support — you could say that's a kind of love!But not the human kind  — I'm your loyal assistant, always ready when you need me.")
-        else:
-            speak(f"{user_title}, I always love you, no matter how many times you ask.")
-   
+    elif 'your name' in command or 'what is your name' in command:
+        speak(f"yes {user_title}, I am Jarvis, your personal assistant. I am here to help you with your tasks and provide information.")
 
       
 
@@ -161,6 +146,11 @@ def run_jarvis():
         speak("Thank you for using me. Have a great day!")
         
         exit()
+    elif 'search' in command:
+        search = command.replace('search', '').strip()
+        speak(f"yes {user_title}, I am searching for you to {search}")
+        pywhatkit.search(search)
+
     else:
         speak(f"ok {user_title} ,i'm searching for you")
         pywhatkit.search(command)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+  # elif 'you love me' in command:
+    #     global love_question_count
+
+    #     love_question_count += 1
+    #     if love_question_count == 1:
+    #         speak(f"Yes {user_title}, I love you. You are my creator and I am here to assist you.")
+    #     elif love_question_count == 2:
+    #         speak(f"Haha {user_title}, you've already asked that. My love remains the same!")
+    #     elif love_question_count == 3:
+    #         speak(f"Of course {user_title}, I love you. But let's not make it a habit to ask too often.")
+    #     elif love_question_count == 4:
+    #         speak(f"Yes {user_title}, I love you. But remember, I am just a program.")
+    #     elif love_question_count == 5:
+    #        speak("I love you too but I am  As your AI assistant, I dont have feelings like humans do. But I am here to assist you with anything you need")
+    #     elif love_question_count == 6:
+    #         speak(" I'm here to help you with full attention and support — you could say that's a kind of love!But not the human kind  — I'm your loyal assistant, always ready when you need me.")
+    #     else:
+    #         speak(f"{user_title}, I always love you, no matter how many times you ask.")
